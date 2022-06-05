aef931d5b3c8a6df252e909548deb1f6889cd825
Retrontology <retrontology@hotmail.com>
2022-06-05 02:50:24 -0500

    loop

--- audio.lua
+++ audio.lua
@@ -1,14 +1,27 @@
-local dfpwm = require("cc.audio.dfpwm")
-local speaker = peripheral.find("speaker")
+function main()
+    local file = arg[1]
+    local volume = tonumber(arg[2])
+    loop(file, volume)
+end
 
-local file = arg[1]
-local volume = tonumber(arg[2])
+function play(file, volume)
+    local dfpwm = require("cc.audio.dfpwm")
+    local speaker = peripheral.find("speaker")
 
-local decoder = dfpwm.make_decoder()
-for chunk in io.lines(file, 16 * 1024) do
-    local buffer = decoder(chunk)
+    local decoder = dfpwm.make_decoder()
+    for chunk in io.lines(file, 16 * 1024) do
+        local buffer = decoder(chunk)
 
-    while not speaker.playAudio(buffer, volume) do
-        os.pullEvent("speaker_audio_empty")
+        while not speaker.playAudio(buffer, volume) do
+            os.pullEvent("speaker_audio_empty")
+        end
     end
 end
+
+function loop(file, volume)
+    while true do
+        play(file, volume)
+    end
+end
+
+main()
