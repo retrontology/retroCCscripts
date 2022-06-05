c90005369193135f3f1fcbe3674a6d9031e15fde
Retrontology <retrontology@hotmail.com>
2022-06-05 04:25:37 -0500

    test

--- play_gif.lua
+++ play_gif.lua
@@ -6,7 +6,6 @@ function main()
 end
 
 function play(file)
-    local mon = peripheral.find("monitor")
     local gifs = fs.find("*.gif")  -- Note: fs.find() is case-sensitive, even if your file system isn't.
 
     mon.setTextScale(0.5)
@@ -16,15 +15,7 @@ function play(file)
     mon.setBackgroundColour(image[1].transparentCol or image.backgroundCol)
     mon.clear()
     
-    parallel.waitForAny(
-            function()
-                GIF.animateGIF(image, math.floor((x - image.width) / 2) + 1, math.floor((y - image.height) / 2) + 1, mon)
-            end,
-            
-            function()
-                    sleep(10)
-            end
-    )
+    GIF.animateGIF(image)
 
 end
 
