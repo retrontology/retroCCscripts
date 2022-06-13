0ad2851ce9bbc23baa8cf647c47dacca98249bf0
Retrontology <retrontology@hotmail.com>
2022-06-12 19:31:03 -0500

    test

--- drop_cobble.lua
+++ drop_cobble.lua
@@ -1,6 +1,8 @@
+local rt = require "retroturtle"
+
 while true do
     local event = os.pullEvent("turtle_inventory")
-    local junk = find_junk()
+    local junk = rt.find_junk()
     for k,v in pairs(junk) do
         turtle.select(v.index)
         turtle.drop()
