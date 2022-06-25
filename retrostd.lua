7146a85d27f1dbfe67d29e6cc070567df3814a13
Retrontology <retrontology@hotmail.com>
2022-06-25 02:56:25 -0500

    distance funcs

--- retrostd.lua
+++ retrostd.lua
@@ -39,3 +39,11 @@ function run_subroutines(subroutines)
         end
     end
 end
+
+function calculate_distance(x1, y1, z1, x2, y2, z2)
+    return math.abs(x2-x1) + math.abs(y2-y1) + math.abs(z2-z1)
+end
+
+function calculate_direct_distance(x1, y1, z1, x2, y2, z2)
+    return math.sqrt((x2-x1)^2 + (y2-y1)^2 + (z2-z1)^2)
+end
