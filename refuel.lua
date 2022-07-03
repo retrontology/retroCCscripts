b8b9ae90d4b689880f74b14c5997fb9be6e301b3
Retrontology <retrontology@hotmail.com>
2022-07-03 02:54:50 -0500

    fix refuel

--- refuel.lua
+++ refuel.lua
@@ -2,7 +2,10 @@ FUEL = "minecraft:lava"
 
 function main()
     local refills = tonumber(arg[1])
-    local dir = string.lower(arg[2])
+    local dir = arg[2]
+    if dir ~= nil then
+        dir = string.lower(arg[2])
+    end
     local place_func = turtle.place
     local inspect_func = turtle.inspect
     if dir == 'up' then
@@ -14,7 +17,7 @@ function main()
     end
     for i=1,refills do
         local has_block, data = inspect_func()
-        while not has_black or data.name != FUEL do
+        while not has_black or data.name ~= FUEL do
             sleep(0.05)
             has_block, data = inspect_func()
         end
