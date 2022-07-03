ff25ada186d099b9c893fb0d52f67cd0982e5920
Retrontology <retrontology@hotmail.com>
2022-07-03 02:44:27 -0500

    wait for lava

--- refuel.lua
+++ refuel.lua
@@ -1,15 +1,23 @@
+FUEL = "minecraft:lava"
+
 function main()
     local refills = tonumber(arg[1])
     local dir = string.lower(arg[2])
-    local place_func = nil
+    local place_func = turtle.place
+    local inspect_func = turtle.inspect
     if dir == 'up' then
         place_func = turtle.placeUp
+        inspect_func = turtle.inspectUp
     elseif dir == 'down' then
         place_func = turtle.placeDown
-    else
-        place_func = turtle.place
+        inspect_func = turtle.inspectDown
     end
     for i=1,refills do
+        local has_block, data = inspect_func()
+        while not has_black or data.name != FUEL do
+            sleep(0.05)
+            has_block, data = inspect_func()
+        end
         place_func()
         turtle.refuel()
         print('Current fuel: ' .. turtle.getFuelLevel())
