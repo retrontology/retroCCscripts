b580b1e6c25672f63c373430670bfda13f152d7f
Retrontology <retrontology@hotmail.com>
2022-07-03 02:32:03 -0500

    up and down

--- refuel.lua
+++ refuel.lua
@@ -1,7 +1,16 @@
 function main()
-    refills = tonumber(arg[1])
+    local refills = tonumber(arg[1])
+    local dir = string.lower(arg[2])
+    local place_func = nil
+    if dir == 'up' then
+        place_func = turtle.placeUp
+    elseif dir == 'down' then
+        place_func = turtle.placeDown
+    else
+        place_func = turtle.place
+    end
     for i=1,refills do
-        turtle.place()
+        place_func()
         turtle.refuel()
     end
 end
