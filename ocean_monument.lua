2da1c1f75da12961c950d34ab0308e8a641459b3
Retrontology <retrontology@hotmail.com>
2022-07-16 03:08:33 -0500

    respect ooo

--- ocean_monument.lua
+++ ocean_monument.lua
@@ -117,7 +117,7 @@ function fill_monument(x, z, offset)
     go_directly_to(x+offset, SEA_LEVEL + 1, z)
     face_direction(DIRECTIONS.EAST)
     for i=1+offset,58 do
-        if i-offset % 2 == 1 then
+        if (i-offset) % 2 == 1 then
             turn_right()
         else
             turn_left()
@@ -129,7 +129,7 @@ function fill_monument(x, z, offset)
         end
         fill_sand()
         if i < 58 then
-            if i-offset % 2 == 1 then
+            if (i-offset) % 2 == 1 then
                 turn_left()
             else
                 turn_right()
