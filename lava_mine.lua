dedafc3c5ef863b011381fad3a92fc77124f7cdf
Retrontology <retrontology@hotmail.com>
2022-07-27 20:13:35 -0500

    progress

--- lava_mine.lua
+++ lava_mine.lua
@@ -3,28 +3,47 @@ require "retroturtle"
 BUCKET = 'minecraft:bucket'
 WATER_BUCKET = 'minecraft:water_bucket'
 DEFAULT_DELAY = 3
-CUTOFF = 20
+
+SUBROUTINES = {
+    drop_cobble = {}
+}
 
 function main()
-    local not_obs = 0
+    run_subroutines(SUBROUTINES)
     while true do
+        if row_count % 2 == 0 then
+            face_direction(DIRECTIONS.EAST)
+        else
+            face_direction(DIRECTIONS.WEST)
+        end
         local has_block, data = turtle.inspectDown()
         if has_block then
             if data.name == 'minecraft:lava' then
                 douse()
                 has_block, data = turtle.inspectDown()
             end
-            if data.name == 'minecraft:obsidian' then
+            if data.name == 'minecraft:obsidian' or data.name == 'minecraft:cobblestone' then
                 mine_down()
-                not_obs = 0
                 if inv_full() then
                     error_return('Inventory full!')
                 end
+                mine_forward()
+                move_forward()
             else
-                not_obs = not_obs + 1
-            end
-            if not_obs > CUTOFF then
-                error_return('Reached ' .. CUTOFF .. ' blocks in a row without finding obsidian...')
+                move_backward()
+                face_direction(DIRECTIONS.NORTH)
+                move_forward()
+                row_count += 1
+
+
+                
+                if row_count % 2 == 0 then
+                    face_direction(DIRECTIONS.EAST)
+                else
+                    face_direction(DIRECTIONS.WEST)
+                end
+                has_block, data = turtle.inspectDown()
+                
             end
         end
     end
@@ -49,6 +68,10 @@ end
 function error_return(err)
     go_directly_to(0, 0, 0)
     error(err)
+end
+
+function inv_full()
+    return false
 end
 
 main()
